f0dbb9db6a8c407af10ec51a8f0f72c8bd1b9f5e
PatBanks <pat.fairbanks1@gmail.com>
2020-12-03 19:36:31 -0800

    Day 3, P1 &2

---
+++ Day3/Day3P1.py
@@ -0,0 +1,45 @@
+from Parser.Parser import Parser
+from math import ceil, floor
+
+#x is the number of row jumps,
+#y is the number of column jumps
+def countTrees(input, x, y):
+    trees = 0
+    columns = len(input[0])
+    print("Columns are: " + str(columns))
+    for i in range(x, len(input), x):
+        if x > y:
+            if input[i][int(i/(y+1)) % (columns-1)] == '#':
+                trees += 1
+        else:
+            if input[i][(i*y) % (columns-1)] == '#':
+                trees += 1
+        print(trees)
+    return trees
+
+def multiply_runs(input_vals: list, x: list, y: list) -> int:
+    trees = 1
+    if len(x) != len(y):
+        return 0
+    else:
+        for i in range(len(x)):
+            trees = trees * countTrees((input_vals), x[i], y[i])
+    return trees
+
+if __name__ == '__main__':
+    parse_me = Parser(r"C:\Users\patfa\PycharmProjects\AdventOfCode\Day3\Day3P1Input")
+    # problem 1
+    tree_count = countTrees(parse_me.Get_Input(), 1, 3)
+    print(tree_count)
+    test = ['..##........',
+            '#...#...#..#',
+            '.#....#..#..',
+            '..#.#...#.#.',
+            '.#...##..#..',
+            '..#.##......',
+            '.#.#.#....#.',
+            '.#........#.',
+            '#.##...#...#',
+            '#...##....##',
+            '.#..#...#.#.']
+    print(multiply_runs(parse_me.Get_Input(), [1, 1, 1, 1, 2], [1, 3, 5, 7, 1]))
